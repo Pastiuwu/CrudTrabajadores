54939e27a24a87283265c8df7f8a45cd797f268f
Pastiuwu <118398130+Pastiuwu@users.noreply.github.com>
2024-10-30 08:56:57 -0300

    Update models.py

--- CrudTrabajadores/models.py
+++ CrudTrabajadores/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
-def validate_active_status(value):
-    if not isinstance(value, bool):
-        raise ValidationError("BORRAR VALIDACION XD")
+#def validate_active_status(value):
+#    if not isinstance(value, bool):
+#        raise ValidationError("BORRAR VALIDACION XD")
 
 class Trabajador(models.Model):
     nombre = models.CharField(max_length=50, blank=False)
